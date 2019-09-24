e132b3ef2fae0104874ace14eb0070450d608975
yjchen5 <yjchen5@nuvoton.com>
2019-09-24 14:26:06 +0800

    Add ISP_DFU, ISP_HID and ISP_SPI sample code.
    Change-Id: Ib199773ecdfee2675039913953f58c9d5321cd7d

---
+++ SampleCode/ISP/ISP_HID/startup_NUC126_user.s
@@ -0,0 +1,191 @@
+;/**************************************************************************//**
+; * @file     startup_NUC126_user.s
+; * @version  V2.00
+; * @brief    NUC126 Series Startup Source File
+; *
+; * @note
+; * Copyright (C) 2019 Nuvoton Technology Corp. All rights reserved.
+; *
+; ******************************************************************************/
+    IF :LNOT: :DEF: Stack_Size
+Stack_Size      EQU     0x00000400
+    ENDIF
+
+                AREA    STACK, NOINIT, READWRITE, ALIGN=3
+Stack_Mem       SPACE   Stack_Size
+__initial_sp
+
+
+; <h> Heap Configuration
+;   <o>  Heap Size (in Bytes) <0x0-0xFFFFFFFF:8>
+; </h>
+    IF :LNOT: :DEF: Heap_Size
+Heap_Size       EQU     0x00000000
+    ENDIF
+                AREA    HEAP, NOINIT, READWRITE, ALIGN=3
+__heap_base
+Heap_Mem        SPACE   Heap_Size
+__heap_limit
+
+
+                PRESERVE8
+                THUMB
+
+
+; Vector Table Mapped to Address 0 at Reset
+                AREA    RESET, DATA, READONLY
+                EXPORT  __Vectors
+
+__Vectors       DCD     __initial_sp              ; Top of Stack
+                DCD     Reset_Handler             ; Reset Handler
+                DCD     NMI_Handler               ; NMI Handler
+                DCD     HardFault_Handler         ; Hard Fault Handler
+                DCD     0                         ; Reserved
+                DCD     0                         ; Reserved
+                DCD     0                         ; Reserved
+                DCD     0                         ; Reserved
+                DCD     0                         ; Reserved
+                DCD     0                         ; Reserved
+                DCD     0                         ; Reserved
+                DCD     SVC_Handler               ; SVCall Handler
+                DCD     0                         ; Reserved
+                DCD     0                         ; Reserved
+                DCD     PendSV_Handler            ; PendSV Handler
+                DCD     SysTick_Handler           ; SysTick Handler
+
+                ; External Interrupts
+
+                AREA    |.text|, CODE, READONLY
+                
+                
+                
+; Reset Handler 
+                
+                ENTRY
+                
+Reset_Handler   PROC
+                EXPORT  Reset_Handler             [WEAK]
+                IMPORT  SystemInit
+                IMPORT  __main
+                
+                LDR     R0, =SystemInit
+                BLX     R0
+                LDR     R0, =__main
+                BX      R0
+                ENDP
+
+                
+                
+; Dummy Exception Handlers (infinite loops which can be modified)
+                
+NMI_Handler     PROC
+                EXPORT  NMI_Handler               [WEAK]
+                B       .
+                ENDP
+HardFault_Handler\
+                PROC
+                EXPORT  HardFault_Handler         [WEAK]
+                B       .
+                ENDP
+SVC_Handler     PROC
+                EXPORT  SVC_Handler               [WEAK]
+                B       .
+                ENDP
+PendSV_Handler  PROC
+                EXPORT  PendSV_Handler            [WEAK]
+                B       .
+                ENDP
+SysTick_Handler PROC
+                EXPORT  SysTick_Handler           [WEAK]
+                B       .
+                ENDP
+
+Default_Handler PROC
+
+                EXPORT  BOD_IRQHandler            [WEAK]
+                EXPORT  WDT_IRQHandler            [WEAK]
+                EXPORT  EINT024_IRQHandler        [WEAK]
+                EXPORT  EINT135_IRQHandler        [WEAK]
+                EXPORT  GPAB_IRQHandler           [WEAK]
+                EXPORT  GPCDEF_IRQHandler         [WEAK]
+                EXPORT  PWM0_IRQHandler           [WEAK]
+                EXPORT  PWM1_IRQHandler           [WEAK]
+                EXPORT  TMR0_IRQHandler           [WEAK]
+                EXPORT  TMR1_IRQHandler           [WEAK]
+                EXPORT  TMR2_IRQHandler           [WEAK]
+                EXPORT  TMR3_IRQHandler           [WEAK]
+                EXPORT  UART02_IRQHandler         [WEAK]
+                EXPORT  UART1_IRQHandler          [WEAK]
+                EXPORT  SPI0_IRQHandler           [WEAK]
+                EXPORT  SPI1_IRQHandler           [WEAK]
+                EXPORT  I2C0_IRQHandler           [WEAK]
+                EXPORT  I2C1_IRQHandler           [WEAK]
+				EXPORT  USCI_IRQHandler           [WEAK]
+                EXPORT  USBD_IRQHandler           [WEAK]
+                EXPORT  SC01_IRQHandler           [WEAK]
+                EXPORT  ACMP01_IRQHandler         [WEAK]
+                EXPORT  PDMA_IRQHandler           [WEAK]
+                EXPORT  PWRWU_IRQHandler          [WEAK]
+                EXPORT  ADC_IRQHandler            [WEAK]
+                EXPORT  CLKDIRC_IRQHandler        [WEAK]
+                EXPORT  RTC_IRQHandler            [WEAK]
+                
+BOD_IRQHandler
+WDT_IRQHandler
+EINT024_IRQHandler
+EINT135_IRQHandler
+GPAB_IRQHandler
+GPCDEF_IRQHandler
+PWM0_IRQHandler
+PWM1_IRQHandler
+TMR0_IRQHandler
+TMR1_IRQHandler
+TMR2_IRQHandler
+TMR3_IRQHandler
+UART02_IRQHandler
+UART1_IRQHandler
+SPI0_IRQHandler
+SPI1_IRQHandler
+I2C0_IRQHandler
+I2C1_IRQHandler
+USCI_IRQHandler
+USBD_IRQHandler
+SC01_IRQHandler
+ACMP01_IRQHandler
+PDMA_IRQHandler
+PWRWU_IRQHandler
+ADC_IRQHandler
+CLKDIRC_IRQHandler
+RTC_IRQHandler
+                B       .
+                ENDP
+
+
+                ALIGN
+
+
+; User Initial Stack & Heap
+
+                IF      :DEF:__MICROLIB
+                
+                EXPORT  __initial_sp
+                EXPORT  __heap_base
+                EXPORT  __heap_limit
+                
+                ELSE
+                
+                IMPORT  __use_two_region_memory
+                EXPORT  __user_initial_stackheap
+__user_initial_stackheap
+
+                LDR     R0, =  Heap_Mem
+                LDR     R1, = (Stack_Mem + Stack_Size)
+                LDR     R2, = (Heap_Mem +  Heap_Size)
+                LDR     R3, = Stack_Mem
+                BX      LR
+
+                ALIGN
+
+                ENDIF
+                
+                END
