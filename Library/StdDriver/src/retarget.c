4116796d6a23266daf81f4f39315dfb10692f05b
phtseng <phtseng@nuvoton.com>
2019-11-12 09:17:28 +0800

    Append \r char before sending \n when use printf
    Change-Id: I27d99c649bac1a2ed73442aa1b087d7772970481

--- Library/StdDriver/src/retarget.c
+++ Library/StdDriver/src/retarget.c
@@ -438,12 +438,14 @@ void SendChar_ToUART(int ch)
 {
 
     while(DEBUG_PORT->FIFOSTS & UART_FIFOSTS_TXFULL_Msk);
-    DEBUG_PORT->DAT = ch;
+
     if(ch == '\n')
     {
-        while(DEBUG_PORT->FIFOSTS & UART_FIFOSTS_TXFULL_Msk);
         DEBUG_PORT->DAT = '\r';
+        while(DEBUG_PORT->FIFOSTS & UART_FIFOSTS_TXFULL_Msk);
     }
+
+    DEBUG_PORT->DAT = ch;
 }
 #else
 
@@ -460,14 +462,6 @@ void SendChar_ToUART(int ch)
     if(ch)
     {
         // Push char
-        i32Tmp = i32Head + 1;
-        if(i32Tmp >= BUF_SIZE) i32Tmp = 0;
-        if(i32Tmp != i32Tail)
-        {
-            u8Buf[i32Head] = ch;
-            i32Head = i32Tmp;
-        }
-
         if(ch == '\n')
         {
             i32Tmp = i32Head + 1;
@@ -478,6 +472,15 @@ void SendChar_ToUART(int ch)
                 i32Head = i32Tmp;
             }
         }
+
+        i32Tmp = i32Head + 1;
+        if(i32Tmp >= BUF_SIZE) i32Tmp = 0;
+        if(i32Tmp != i32Tail)
+        {
+            u8Buf[i32Head] = ch;
+            i32Head = i32Tmp;
+        }
+
     }
     else
     {
@@ -663,12 +666,12 @@ int _write (int fd, char *ptr, int len)
     while(i--) {
         while(DEBUG_PORT->FIFOSTS & UART_FIFOSTS_TXFULL_Msk);
 
-        DEBUG_PORT->DAT = *ptr++;
-
         if(*ptr == '\n') {
-            while(DEBUG_PORT->FIFOSTS & UART_FIFOSTS_TXFULL_Msk);
             DEBUG_PORT->DAT = '\r';
+            while(DEBUG_PORT->FIFOSTS & UART_FIFOSTS_TXFULL_Msk);
         }
+
+        DEBUG_PORT->DAT = *ptr++;
     }
     return len;
 }
